4d028d4470166b007aa4ef676368545d7795afdf
dratizzo <111133664+dratizzo@users.noreply.github.com>
2025-10-23 18:19:33 -0300

    Primeira versão pedra, papel e tesoura funcional, adição das funções jogar rodadas, verificar escolha e contabilizar vitórias, com chamada para serem jogadas cinco rodadas

--- javascript.js
+++ javascript.js
@@ -1,23 +1,66 @@
-console.log("Hello, World")
-
 function getComputerChoice() {
 
     randInt = Math.floor(Math.random() * 3)
 
     if (randInt === 0) {
-        return "rock";
+        let escolhaComputador = "rock";
+        return escolhaComputador;
     }
 
     if (randInt === 1) {
-        return "paper";
+        let escolhaComputador = "paper";
+        return escolhaComputador;
     } else {
-        return "scissors";
+        let escolhaComputador = "scissors";
+        return escolhaComputador;
     }
 
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    let escolhaHumano = prompt("Choose 'rock','paper' or 'scissors'!")
+    return escolhaHumano;
+}
 
-//function getHumanChoice() {
+function playGame() {
+    
+    // Inicialização das variáveis de pontuação
+    let humanScore = 0;
+    let computerScore = 0;
 
-//}
+    function playRound(humanChoice, computerChoice) {
+        
+        humanChoice = humanChoice.toLowerCase();
+        computerChoice = computerChoice.toLowerCase();
+
+        console.log("Escolha humano:", humanChoice);
+        console.log("Escolha máquina:", computerChoice);
+
+        // Caso seja empate
+        if (humanChoice === computerChoice) {
+            console.log("Drawn!");
+        }
+
+        // Casos que a máquina vence
+        if (humanChoice == "rock" && computerChoice == "paper" || humanChoice == "scissors" && computerChoice == "rock" || humanChoice == "paper" && computerChoice == "scissors") {
+            computerScore++;
+            console.log("E começa a era das máquinas!");
+        }
+
+        // Casos que o humano vence
+        if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "scissors" && computerChoice == "paper" || humanChoice == "paper" && computerChoice == "rock") {
+            humanScore++;
+            console.log("Humanos superiores!");
+        }
+    }
+
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+
+    console.log("Pontuação humano:", humanScore);
+    console.log("Pontuação máquina:", computerScore);
+
+}
