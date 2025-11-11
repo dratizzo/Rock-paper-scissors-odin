dc1bbf8f527c68bb74c30130709514df725a3f80
dratizzo <111133664+dratizzo@users.noreply.github.com>
2025-11-11 10:14:05 -0300

    Refatoração para criação de interface simples e lógica passada por manipulação do DOM

--- javascript.js
+++ javascript.js
@@ -3,23 +3,12 @@ function getComputerChoice() {
     randInt = Math.floor(Math.random() * 3)
 
     if (randInt === 0) {
-        let escolhaComputador = "rock";
-        return escolhaComputador;
-    }
-
-    if (randInt === 1) {
-        let escolhaComputador = "paper";
-        return escolhaComputador;
+        return 'rock';
+    } else if (randInt === 1) {
+        return 'paper';
     } else {
-        let escolhaComputador = "scissors";
-        return escolhaComputador;
+        return 'scissors';
     }
-
-}
-
-function getHumanChoice() {
-    let escolhaHumano = prompt("Choose 'rock','paper' or 'scissors'!")
-    return escolhaHumano;
 }
 
 function playGame() {
@@ -29,39 +18,56 @@ function playGame() {
     let computerScore = 0;
 
     function playRound(humanChoice, computerChoice) {
-        
-        humanChoice = humanChoice.toLowerCase();
-        computerChoice = computerChoice.toLowerCase();
 
         console.log("Escolha humano:", humanChoice);
         console.log("Escolha máquina:", computerChoice);
         console.log("-------------------------------")
 
-        // Caso seja empate
-        if (humanChoice === computerChoice) {
-            console.log("Drawn!");
-        }
+        pRoundWinner = document.createElement("p");
 
-        // Casos que a máquina vence
         if (humanChoice == "rock" && computerChoice == "paper" || humanChoice == "scissors" && computerChoice == "rock" || humanChoice == "paper" && computerChoice == "scissors") {
             computerScore++;
-            console.log("E começa a era das máquinas!");
+            pRoundWinner.textContent = "E começa a era das máquinas!";
+        } else if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "scissors" && computerChoice == "paper" || humanChoice == "paper" && computerChoice == "rock") {
+            humanScore++;
+            pRoundWinner.textContent = "Humanos superiores!";
+        } else {
+            console.log("Drawn!");
+            pRoundWinner.textContent = "Empate!";
         }
 
-        // Casos que o humano vence
-        if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "scissors" && computerChoice == "paper" || humanChoice == "paper" && computerChoice == "rock") {
-            humanScore++;
-            console.log("Humanos superiores!");
+        if (humanScore >= 5) {
+            pRoundWinner.textContent = "Vencedor final: humano!";
+            humanScore = 0;
+            computerScore = 0;
+        } else if (computerScore >= 5) {
+            pRoundWinner.textContent = "Vencedor final: máquina!";
+            humanScore = 0;
+            computerScore = 0;
         }
+
+        const displayDiv = document.querySelector("div");
+        displayDiv.appendChild(pRoundWinner);
+
+        console.log("Pontuação humano:", humanScore);
+        console.log("Pontuação máquina:", computerScore);
     }
 
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
+    let btnRock = document.querySelector("#rock")
+    const btnPaper = document.querySelector(".paper")
+    const btnScissors = document.querySelector(".scissors")
 
-    console.log("Pontuação humano:", humanScore);
-    console.log("Pontuação máquina:", computerScore);
+    btnRock.addEventListener('click', () => {
+        console.log('chega aqui');
+        playRound('rock', getComputerChoice());
+    });
+
+    btnPaper.addEventListener('click', () => {
+        playRound('paper', getComputerChoice());
+    });
+
+    btnScissors.addEventListener('click', () => {
+        playRound('scissors', getComputerChoice());
+    });
 
 }
